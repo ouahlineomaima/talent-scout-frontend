4c241152d45afa08a1aae2d80a435a56e11582a3
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-11-28 01:01:16 +0100

    Adding homa page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <NavBar></NavBar>
+      <Home></Home>
     </Router>
     
       
